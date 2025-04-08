250a79287d086edf3097b2f6ea5fc5189581198d
PrathamBhamare <pgbhamare_b23@it.vjti.ac.in>
2025-04-08 11:14:13 +0530

    Initial commit: React Stdlib Showcase project

---
+++ react-stdlib-showcase/src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+const Header: React.FC = () => {
+  return (
+    <header className="header">
+      <div className="header-content">
+        <h1>JavaScript Standard Library Showcase</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
